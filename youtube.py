0fca1544b1e6d7aa3fa76133b007ee51953f2e67
Kyle Konrad <kyle@plotwatt.com>
2011-07-22 01:14:15 -0400

    refactored youtube code. use python api for youtube queries and playlist management

--- youtube.py
+++ youtube.py
@@ -1,6 +1,5 @@
 """
-basic youtube functionalities to search for a video
-currently uses http api. need to switch to python api to make things cleaner
+basic youtube functionalities to search for a video and add it to a playlist
 Kyle Konrad 7/20/2011
 """
 #TODO: use python api to make this cleaner
@@ -8,29 +7,51 @@ Kyle Konrad 7/20/2011
 import urllib, urllib2, re
 from BeautifulSoup import BeautifulStoneSoup
 
+
+import gdata.youtube
+import gdata.youtube.service
+
+yt_service = gdata.youtube.service.YouTubeService()
+yt_service.ssl = True
+yt_service.client_id = 'AlbumFinder'
+
+developer_key_filename = 'developer_key.txt'
+developer_key_file = open(developer_key_filename)
+yt_service.developer_key = developer_key_file.readline()[:-1] # drop trailing newline
+developer_key_file.close()
+
+api_playlist_base = 'http://gdata.youtube.com/feeds/api/playlists/'
+playlist_base = 'http://youtube.com/playlist?list=PL'
 api_url = 'https://gdata.youtube.com/feeds/api/videos'
-key = 'AI39si6-uhxXoLQaPZsG0ilKLSr2ZPbdSSgektqhFl8sxNcOP86UgCEQfWEdDigd3efGIkBTOt9YE2QxJ4n4Q_AYXSl35WsTlw'
 authsubsessiontoken_url = 'https://www.google.com/accounts/AuthSubSessionToken'
 authsubrequest_url = 'https://www.google.com/accounts/AuthSubRequest'
 
 def query(search_term):
     """search youtube
-    returns query result as raw xml"""
-    params = urllib.urlencode({'q' : search_term, 'key' : key})
-    res = urllib2.urlopen("%s?%s" % (api_url, params)).read() # do it this way so it's a GET
-    return res
-
-def first_video(raw_xml):
-    """get id of first video in search results"""
-    xml = BeautifulStoneSoup(raw_xml)
-    title_tag = xml.findAll('title')[1]
-    link_tag = title_tag.findNextSibling('link')
-    link = link_tag['href']
-    match = re.search('\?v=([\w\-_]+)', link)
-    video_id = match.group(1)
-    return video_id
-
-def get_video(search_term):
-    """get first video from results for search_term"""
-    return first_video(query(search_term))
+    returns id of first result"""
+    query = gdata.youtube.service.YouTubeVideoQuery()
+    query.vq = search_term
+    feed = yt_service.YouTubeQuery(query)
+    return feed.entry[0].id.text
     
+def add_playlist(title, summary):
+    """create a playlist and return its ID"""
+    raw_xml = yt_service.AddPlaylist(title, summary)
+    match = re.search('<ns0:id>.*/([^/]+)</ns0:id>', str(raw_xml))
+    if match:
+        return match.group(1)
+    else:
+        raise Exception('could not parse playlist URL')
+
+def upgrade_token(one_time_token):
+    """upgrade a one-time token to a session token"""
+    yt_service.SetAuthSubToken(one_time_token)
+    try:
+        session_token = yt_service.UpgradeToSessionToken()
+    except Exception as e:
+        return 'failed to created session token: %s' % str(e)
+
+def add_video_to_playlist(video_id, playlist_id):
+    playlist_url = api_playlist_base + playlist_id
+    video_entry = yt_service.AddPlaylistVideoEntryToPlaylist(playlist_url, video_id)
+    #TODO: check success
